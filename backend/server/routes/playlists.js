12b1f3a07317a01e4c29cc5bfcde5a87ec8dca8a
sachin-in1 <saching1998@cet.ac.in>
2020-09-02 16:41:54 +0530

    fresh start

---
+++ backend/server/routes/playlists.js
@@ -0,0 +1,156 @@
+const express = require('express');
+const router = express.Router();
+// const RateLimit = require('express-rate-limit');
+// const mongoose = require('mongoose');
+const Playlist = require('../database/models/playlists');
+
+const minutes = 5;
+// const postLimiter = new RateLimit({
+//   windowMs: minutes * 60 * 1000, // milliseconds
+//   max: 100, // Limit each IP to 100 requests per windowMs 
+//   delayMs: 0, // Disable delaying - full speed until the max limit is reached 
+//   handler: (req, res) => {
+//     res.status(429).json({ success: false, msg: `You made too many requests. Please try again after ${minutes} minutes.` });
+//   }
+// });
+
+router.route('/').get((req, res) => {
+  Playlist.find({})
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.status(500).json({ success: false, msg: `Something went wrong. ${err}` });
+    });
+});
+
+router.get('/:list', (req, res) => {
+  Playlist.findById(req.params.id)
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((err) => {
+        res.status(404).json({ success: false, msg: `No such playlist.` });
+      });
+  });
+
+
+  router.route('/').post(postLimiter, (req, res) => {
+    console.log("hii");
+    let newPlaylist = new Playlist({
+      list: sanitizeName(req.body.list),
+      listtype: sanitizeEmail(req.body.listtype),
+    });
+  
+    newPlaylist.save()
+      .then((result) => {
+        res.json({
+          success: true,
+          msg: `Successfully added!`,
+          result: {
+            _id: result._id,
+            list: result.list,
+            listtype: result.listtype
+          }
+        });
+      })
+      .catch((err) => {
+        if (err.errors) {
+          if (err.errors.list) {
+            res.status(400).json({ success: false, msg: err.errors.list.message });
+            return;
+          }
+          if (err.errors.listtype) {
+            res.status(400).json({ success: false, msg: err.errors.listtype.message });
+            return;
+          }
+          // Show failed if all else fails for some reasons
+          res.status(500).json({ success: false, msg: `Something went wrong. ${err}` });
+        }
+      });
+  });
+  
+  // UPDATE
+  router.put('/:id', (req, res) => {
+  
+    let updatedPlaylist = {
+      list: sanitizeName(req.body.list),
+      listtype: sanitizeEmail(req.body.listtype)
+    };
+  
+    Playlist.findOneAndUpdate({ _id: req.params.id }, updatedPlaylist, { runValidators: true, context: 'query' })
+      .then((oldResult) => {
+        Playlist.findOne({ _id: req.params.id })
+          .then((newResult) => {
+            res.json({
+              success: true,
+              msg: `Successfully updated!`,
+              result: {
+                _id: newResult._id,
+                list: newResult.list,
+                listtype: newResult.listtype
+              }
+            });
+          })
+          .catch((err) => {
+            res.status(500).json({ success: false, msg: `Something went wrong. ${err}` });
+            return;
+          });
+      })
+      .catch((err) => {
+        if (err.errors) {
+          if (err.errors.list) {
+            res.status(400).json({ success: false, msg: err.errors.list.message });
+            return;
+          }
+          if (err.errors.listtype) {
+            res.status(400).json({ success: false, msg: err.errors.listtype.message });
+            return;
+          }
+          // Show failed if all else fails for some reasons
+          res.status(500).json({ success: false, msg: `Something went wrong. ${err}` });
+        }
+      });
+  });
+  
+  // DELETE
+  router.delete('/:id', (req, res) => {
+  
+    Playlist.findByIdAndRemove(req.params.id)
+      .then((result) => {
+        res.json({
+          success: true,
+          msg: `It has been deleted.`,
+          result: {
+            _id: result._id,
+            list: result.list,
+            listtype: result.listtype
+          }
+        });
+      })
+      .catch((err) => {
+        res.status(404).json({ success: false, msg: 'Nothing to delete.' });
+      });
+  });
+  
+  module.exports = router;
+  
+  // Minor sanitizing to be invoked before reaching the database
+  sanitizeList = (list) => {
+    return list.toLowerCase();
+  }
+  sanitizeEmail = (listtype) => {
+    return listtype.toLowerCase();
+  }
+//   sanitizeAge = (age) => {
+//     // Return empty if age is non-numeric
+//     if (isNaN(age) && age != '') return '';
+//     return (age === '') ? age : parseInt(age);
+//   }
+//   sanitizeGender = (gender) => {
+//     // Return empty if it's neither of the two
+//     return (gender === 'm' || gender === 'f') ? gender : '';
+//   }
+  
+/* GET home page. */
+// router.get('/', playlist.getPlaylists());
---
+++ backend/server/server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const cors = require('cors')
+// const session = require('express-session')
+const dbConnection = require('./database') 
+// const MongoStore = require('connect-mongo')(session)
+const passport = require('./passport');
+
+const app = express()
+const PORT = process.env.PORT || 8081
+// Route requires
+// MIDDLEWARE
+app.use(cors())
+app.use(express.json())
+// app.use(bodyParser.json())
+
+// // Sessions
+// app.use(
+// 	session({
+// 		secret: 'fraggle-rock', //pick a random string to make the hash that is generated secure
+// 		store: new MongoStore({ mongooseConnection: dbConnection }),
+// 		resave: false, //required
+// 		saveUninitialized: false //required
+// 	})
+// )
+
+// Passport
+// app.use(passport.initialize())
+// app.use(passport.session()) // calls the deserializeUser
+
+
+// Routes
+const user = require('./database/models/user')
+const playlist = require('./database/models/playlists')
+app.use('/users', user)
+app.use('/playlists', playlist)
+
+// Starting Server 
+app.listen(PORT, () => {
+	console.log(`App listening on PORT: ${PORT}`)
+})
